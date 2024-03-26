30db6d1e7431cea35b15e85a252862a4dd4aaff0
Amani Alharbi <142425670+AmaniSaleh1@users.noreply.github.com>
2024-03-26 09:43:36 +0300

    Add files via upload

--- my_app/apps/jewmodule/urls.py
+++ my_app/apps/jewmodule/urls.py
@@ -1,6 +1,17 @@
+<<<<<<< HEAD
+from django.urls import path, re_path
+from apps.jewmodule.views import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('jewelry', views.jewelry),
+    path('jewelry/<int: jew id>', views.jewelry),
+]
+=======
 from django.urls import path
 from apps.jewmodule.views import index
 
 urlpatterns = [
     path('', index, name='index'),
 ]
+>>>>>>> 65a4a5d96c0f434d53e3290270e7c145a1ddb7f3
--- my_app/apps/jewmodule/views.py
+++ my_app/apps/jewmodule/views.py
@@ -1,5 +1,35 @@
+<<<<<<< HEAD
+from django.shortcuts import render,redirect
+
+def index(request):
+    # Study the request
+    return render(request, 'jewmodule/index.html')  
+# Rendering a template
+def jewelry(request):
+    u = request.user
+    return render(request, 'jewmodule/jewelry.html') 
+
+def jewelry(request, jew id):
+    return None
+
+def jewelry(request, bId):
+    Gemstones1 = {'id': 12344321, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'}
+    Gemstones2 = {'id': 56788765, 'title': 'Reversing: Secrets of Reverse Engineering', 'author': 'E. Ellams'}
+    
+    target = None
+    
+    if Gemstones1['id'] == bId:
+        targetBook = Gemstones1
+    elif Gemstones2['id'] == bId:
+        targetBook = Gemstones2
+    
+    context = {'book': targetBook}  # 'book'
+    return render(request, 'jewmodule/book.html', context)
+
+=======
 from django.shortcuts import render
 
 def index(request):
     # Study the request
     return render(request, 'jewmodule/index.html')  # Rendering a template
+>>>>>>> 65a4a5d96c0f434d53e3290270e7c145a1ddb7f3
