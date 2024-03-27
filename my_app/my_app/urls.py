6326b0605a71849267dc6a65c64daf96845c9dd2
Amani Alharbi <142425670+AmaniSaleh1@users.noreply.github.com>
2024-03-28 02:50:10 +0300

    Add files via upload

--- my_app/my_app/urls.py
+++ my_app/my_app/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include('apps.jewmodule.urls'))
 ]
 =======
+<<<<<<< HEAD
 """
 URL configuration for my_app project.
 
@@ -46,4 +47,29 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.jewmodule.urls'))
 ]
+=======
+"""
+URL configuration for my_app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('apps.jewmodule.urls'))
+]
+>>>>>>> f805db6a1790a2fd31e46ec097a587081675bc65
 >>>>>>> 65a4a5d96c0f434d53e3290270e7c145a1ddb7f3
