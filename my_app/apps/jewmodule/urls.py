1c132abf74eaa1ec28fd730119e1490b3c67c86d
Amani Alharbi <142425670+AmaniSaleh1@users.noreply.github.com>
2024-03-28 06:59:10 +0300

    lec 6&7

--- my_app/apps/jewmodule/urls.py
+++ my_app/apps/jewmodule/urls.py
@@ -1,30 +1,9 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f805db6a1790a2fd31e46ec097a587081675bc65
 from django.urls import path, re_path
-from apps.jewmodule.views import views
+from apps.jewmodule import views
 
-urlpatterns = [
+urlpatterns = [    
     path('', views.index, name='index'),
-    path('jewelry', views.jewelry),
-    path('jewelry/<int: jew id>', views.jewelry),
+    path('jewelry', views.jewelry, name = "jewelry"),
+    path('jewelry/<int:bId>', views.jewelry),
+    path('filter_jewelry', views.filter_jewelry, name="filter_jewelry")
 ]
-<<<<<<< HEAD
-
-from django.urls import path
-from apps.jewmodule.views import index
-
-urlpatterns = [
-    path('', index, name='index'),
-]
-=======
-=======
-from django.urls import path
-from apps.jewmodule.views import index
-
-urlpatterns = [
-    path('', index, name='index'),
-]
->>>>>>> 65a4a5d96c0f434d53e3290270e7c145a1ddb7f3
->>>>>>> f805db6a1790a2fd31e46ec097a587081675bc65
